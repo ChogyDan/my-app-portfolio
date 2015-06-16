c8d5212e523bf470542047e07d4bc427bc6704cf
Daniel Hollocher <danielhollocher@gmail.com>
2015-06-16 10:11:08 -0400

    updates per instructor feedback and a class targeting the next project

---
+++ app/src/main/java/org/example/nanodegreeportfolioapp/Project1Fragment.java
@@ -0,0 +1,23 @@
+package org.example.nanodegreeportfolioapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Project1Fragment extends Fragment {
+
+    public Project1Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_project1, container, false);
+    }
+}
